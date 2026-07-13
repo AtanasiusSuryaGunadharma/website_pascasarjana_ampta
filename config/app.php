6808c3d62ef3a847709631ac373862a8bb74f358
AtanasiusSuryaGunadharma <220711667@students.uajy.ac.id>
2026-07-13 09:54:49 +0800

    Make url can acces without public, so with : http://localhost/website_pascasarjana_ampta/

--- config/app.php
+++ config/app.php
@@ -6,5 +6,5 @@ return [
     'name' => $_ENV['APP_NAME'] ?? 'Pascasarjana STP AMPTA',
     'env' => $_ENV['APP_ENV'] ?? 'production',
     'debug' => filter_var($_ENV['APP_DEBUG'] ?? false, FILTER_VALIDATE_BOOLEAN),
-    'url' => rtrim($_ENV['APP_URL'] ?? 'http://localhost/website_pascasarjana_ampta/public', '/'),
+    'url' => rtrim($_ENV['APP_URL'] ?? 'http://localhost/website_pascasarjana_ampta', '/'),
 ];
